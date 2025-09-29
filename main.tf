8be68281bbc7097044464c2399cb1b209a208446
David Trejos <dtrejos@akamai.com>
2025-09-29 11:49:14 -0600

    Update provider.tf fixing a definition error

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
-data "akamai_group" "trejos" {
+data "akamai_group" "my_group_id" {
      group_name   = "trejos"
      contract_id  = "1-5C13O8"
 }
 
-#data "akamai_property" "test_property" {
-#     property_name   = "tcwest.trejos.info"
-#     contract_id  = "1-5C13O8"
-#}
+output "my_group_id" {
+    value = data.akamai_group.my_group_id
+}
+
+data "akamai_properties" "my_properties" {
+    group_id    = "237657"
+    contract_id = "1-5C13O8"
+}
+
+output "my_properties" {
+  value = data.akamai_properties.my_properties
+}
+
+data "akamai_appsec_configuration" "sec-configurations" {
+     name ="2024_LAB_BrBio"
+}
+
+output "configuration-list" {
+  value = data.akamai_appsec_configuration.sec-configurations.output_text
+}
