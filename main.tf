3a43aede2bfc08d5b75f82cb2bf9b386d71ea4e5
David Trejos <dtrejos@akamai.com>
2025-09-28 21:52:35 -0600

    Add main.tf file

---
+++ main.tf
@@ -0,0 +1,9 @@
+data "akamai_group" "trejos" {
+     group_name   = "trejos"
+     contract_id  = "1-5C13O8"
+}
+
+#data "akamai_property" "test_property" {
+#     property_name   = "tcwest.trejos.info"
+#     contract_id  = "1-5C13O8"
+#}
